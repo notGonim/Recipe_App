2aefb2b6df8655628a7c0245837629fb3ed67328
notGonim <mhmodgonim96@gmail.com>
2021-04-12 03:02:51 +0200

    ADDED : generateHTMLFunc to genereate the html for the res

--- main.js
+++ main.js
@@ -14,20 +14,36 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
     searchQuery = e.target.querySelector('input').value
     fetchApi()
-
-
-
 })
 
 
 async function fetchApi() {
 
-    const baseUrl = `https://api.edamam.com/search?q=pizza&app_id=${app_id}&app_key=${api_key}`
+    const baseUrl = `https://api.edamam.com/search?q=pizza&app_id=${app_id}&app_key=${api_key}&to=20`
     const res = await fetch(baseUrl)
     const data = await res.json()
-    console.log(data)
+    generateHtmlFunc(data.hits)
 
+}
 
+function generateHtmlFunc(res) {
 
+    let generateHtml = ''
+    res.map(res => {
+        generateHtml += `
+        
+        <div class="item">
+             <img src="${res.recipe.image}" alt="">
+            <div class="flex_container">
+                <h1 class="title">This is a Recipe</h1>
+                <a href="#" class="view">View Recipe</a>
+            </div>
+            <p class="item_data">
+                Calories:
+            </p>
+    </div>
+        `
+        searchResultDiv.innerHTML = generateHtml
 
+    })
 }
